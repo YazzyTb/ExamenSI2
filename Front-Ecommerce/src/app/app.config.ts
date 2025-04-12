cc3b8ca776b74df917d50feff53c0c0f4db2ea18
Jazmin Torrez <jazz.tb.99@gmail.com>
2025-04-11 22:40:22 -0400

    permisos

--- Front-Ecommerce/src/app/app.config.ts
+++ Front-Ecommerce/src/app/app.config.ts
@@ -5,6 +5,7 @@ import { BitacoraComponent } from './pages/bitacora/bitacora.component';
 import { EmpleadosComponent } from './pages/empleados/empleados.component';
 import { RolesComponent } from './pages/roles/roles.component';
 import { PermisosComponent } from './pages/permisos/permisos.component';
+import { CrearPermisoComponent } from './pages/permisos/crear-permiso/crear-permiso.component';
 import { EmpleadoDetalleComponent } from './pages/empleados/empleado-detalle/empleado-detalle.component';
 import { EditarComponent } from './pages/empleados/editar/editar.component';
 import {CrearEmpleadoComponent } from './pages/empleados/crear-empleado/crear-empleado.component';
@@ -17,14 +18,14 @@ export const appConfig = {
         path: '',
         component: DashboardLayoutComponent,
         children: [
+          { path: '', redirectTo: 'bitacora', pathMatch: 'full' },
           { path: 'bitacora', component: BitacoraComponent },
+          { path: 'permisos', component: PermisosComponent },
+          {path: 'permisos/crear',component: CrearPermisoComponent},
           { path: 'empleados', component: EmpleadosComponent },
           { path: 'empleados/roles', component: RolesComponent },
-          { path: 'empleados/permisos', component: PermisosComponent },
-          { path: '', redirectTo: 'bitacora', pathMatch: 'full' },
           {path: 'empleados/crear', component: CrearEmpleadoComponent},
-          {path: 'empleados/:id',
-            component: EmpleadoDetalleComponent},
+          {path: 'empleados/:id', component: EmpleadoDetalleComponent},
           {path: 'empleados/editar/:id', component: EditarComponent}
         ]
       }
--- Front-Ecommerce/src/app/pages/permisos/permisos.component.ts
+++ Front-Ecommerce/src/app/pages/permisos/permisos.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-permisos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './permisos.component.html',
-  styleUrl: './permisos.component.css'
+  styleUrls: ['./permisos.component.css']
 })
 export class PermisosComponent {
-
+  permisos = [
+    { id: 'P01', nombre: 'Crear usuario', descripcion: 'Permite registrar nuevos usuarios' },
+    { id: 'P02', nombre: 'Eliminar ventas', descripcion: 'Permite borrar ventas del sistema' },
+    { id: 'P03', nombre: 'Ver reportes', descripcion: 'Accede al módulo de reportes' }
+  ];
 }
