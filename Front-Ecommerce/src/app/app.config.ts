503310e70c673db567ee6193e62d676b1a0904ea
Jazmin Torrez <jazz.tb.99@gmail.com>
2025-04-12 00:07:10 -0400

    Clientes terminado

--- Front-Ecommerce/src/app/app.config.ts
+++ Front-Ecommerce/src/app/app.config.ts
@@ -12,6 +12,10 @@ import { CrearPermisoComponent } from './pages/permisos/crear-permiso/crear-perm
 import { EmpleadoDetalleComponent } from './pages/empleados/empleado-detalle/empleado-detalle.component';
 import { EditarComponent } from './pages/empleados/editar/editar.component';
 import {CrearEmpleadoComponent } from './pages/empleados/crear-empleado/crear-empleado.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { ClienteDetallesComponent } from './pages/clientes/cliente-detalles/cliente-detalles.component';
+import { EditarClienteComponent } from './pages/clientes/editar-cliente/editar-cliente.component';
+
 
 export const appConfig = {
   providers: [
@@ -32,7 +36,10 @@ export const appConfig = {
           { path: 'empleados', component: EmpleadosComponent },
           { path: 'empleados/crear', component: CrearEmpleadoComponent },
           { path: 'empleados/:id', component: EmpleadoDetalleComponent },
-          { path: 'empleados/editar/:id', component: EditarComponent }
+          { path: 'empleados/editar/:id', component: EditarComponent },
+          { path: 'clientes', component: ClientesComponent },
+          { path: 'clientes/:id', component: ClienteDetallesComponent },
+          { path: 'clientes/editar/:id', component: EditarClienteComponent},
         ]
       }
     ])
